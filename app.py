465525d093d08d881084d985720ebb5c61a6144f
adamta12 <adamtajiri6@gmail.com>
2026-03-31 10:04:53 +0100

    cd pipeline

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app=Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello DevOps - Docker CI Pipeline!"
+    return "Hello DevOps - Docker CD PIPELINE!!!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=5000)
